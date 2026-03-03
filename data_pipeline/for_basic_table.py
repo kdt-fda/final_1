c0ef994ffd9ad082befa837743a0c6d8b9734c8f
junbeom <wnsqja0219@naver.com>
2026-03-03 15:50:43 +0900

    is_active 추가(db용)

--- data_pipeline/for_basic_table.py
+++ data_pipeline/for_basic_table.py
@@ -22,9 +22,11 @@ def create_basic(conn):
                     est_dt VARCHAR(20),
                     ipo VARCHAR(20),
                     ind_code VARCHAR(10),
+                    is_active BOOLEAN DEFAULT TRUE,
                     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
                 );
             """) # updated_at 에서 데이터 수정시간 확인 가능
+            # FOREIGN KEY (ind_code) REFERENCES industry(ind_code) 이거 맨 뒷줄에 추가해야 됨 나중에
             print("basic 테이블이 성공적으로 생성되었거나 이미 존재합니다.")
 
     except Exception as e:
@@ -51,21 +53,40 @@ def upload_to_basic():
 
             # 삽입 및 업데이트 => 만약 중복된 키가 발견되면(기본키인 stock_code) 삽입 대신 업데이트
             sql = """
-                INSERT INTO basic (stock_code, corp_code, corp_name, est_dt, ipo, ind_code)
-                VALUES (%s, %s, %s, %s, %s, %s)
+                INSERT INTO basic (stock_code, corp_code, corp_name, est_dt, ipo, ind_code, is_active)
+                VALUES (%s, %s, %s, %s, %s, %s, TRUE)
                 ON DUPLICATE KEY UPDATE
                     corp_code = VALUES(corp_code),
                     corp_name = VALUES(corp_name),
                     est_dt = VALUES(est_dt),
                     ipo = VALUES(ipo),
-                    ind_code = VALUES(ind_code);
+                    ind_code = VALUES(ind_code),
+                    is_active = TRUE;
             """
             
             cursor.executemany(sql, data_to_insert)
             
+            current_stock_codes = df['stock_code'].tolist()
+            
+            if current_stock_codes:
+                # IN 절에 들어갈 %s, %s, ... 문자열 동적 생성
+                format_strings = ','.join(['%s'] * len(current_stock_codes))
+                
+                # 최신 리스트에 없는데 활성화 되어있는 데이터만 비활성화
+                sql_soft_delete = f"""
+                    UPDATE basic 
+                    SET is_active = FALSE 
+                    WHERE stock_code NOT IN ({format_strings}) AND is_active = TRUE;
+                """
+                cursor.execute(sql_soft_delete, tuple(current_stock_codes))
+                soft_deleted_count = cursor.rowcount
+            else:
+                soft_deleted_count = 0
+            
             # 변경사항 확정
             conn.commit()
             print(f"basic table에 적재 완료 ({len(data_to_insert)}건)")
+            print(f"상장폐지 등 크롤링 누락 기업 비활성화(Soft Delete) 처리 완료: {soft_deleted_count}건")
 
     except Exception as e:
         # 오류 발생 시 롤백
