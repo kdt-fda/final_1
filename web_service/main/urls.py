42dc843ddf3474b6ad88336d2545660b28ad6ed8
hg43214321 <hg43214321@gmail.com>
2026-03-11 15:21:43 +0900

    url 정리

--- web_service/main/urls.py
+++ web_service/main/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('overview/', views.overview, name='overview'),
-    path('ai_page/', views.ai_page_preview, name='ai_page_preview'),
     path('ai_page/<str:stock_code>/', views.ai_page, name='ai_page'),
     path('finance/', views.finance, name='finance'),
     path('industry/<str:stock_code>/', views.industry, name='industry'),
