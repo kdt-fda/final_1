28ada6b3339fb49dfb0e2b9e46b55a8bf6fcd375
junbeom <wnsqja0219@naver.com>
2026-03-24 11:44:58 +0900

    settings.py 수정

--- web_service/config/settings.py
+++ web_service/config/settings.py
@@ -4,7 +4,7 @@ import sys
 temp_base = Path(__file__).resolve().parents[2]
 if str(temp_base) not in sys.path:
     sys.path.append(str(temp_base))
-    
+
 from common.setting import db_config, DJANGO_SECRET_KEY
 
 
@@ -17,7 +17,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
